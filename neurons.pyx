d2f7f2badcf501184231e730ca8d63198c23e08d
matthew_green <m.hobson.green@gmail.com>
2017-12-30 13:38:22 -0600

    a nicer neuron

--- neurons.pyx
+++ neurons.pyx
@@ -1,27 +1,48 @@
 
-def izhikevich(double v, double u, double I):
-    cdef:
-        double a = 0.02 
-        double b = 0.2
-        double dv = 0.04*v*v + 5*v + 140 - u + I
-        double du = a * (b*v - u)
-    return dv, du
-
-
-def simulate(int n, double dt, double I):
-    cdef:
-        double v = -65 
-        double u = 0
-        double c = -65
-        double d = 2
-        list record = [None] * n
-    for i in range(n):
-        dv, du = izhikevich(v, u, I)
-        v += dt * dv
-        u += dt * du
-        if v >= 30:
-            v = c
-            u += d
-        record[i] = v
-    return record
+cdef class Neuron:
+    """ A biological model neuron introduced by
+    Izhikevich (2003).
+    """
+    cdef double v, u, I, a, b, c, d
     
+    def __init__(self, v, u, I, a, b, c, d):
+        # State variables are v (observable membrane potential)
+        # and u (hidden recovery variable)
+        self.v = v
+        self.u = u
+
+        # The current I is the net input to the neuron
+        self.I = I
+
+        # These variables control dynamics in various ways
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def evolve(self, double dt):
+        """ Updates state variables u and v, using Euler method.
+        """
+        cdef: 
+            double dv = 0.04*self.v*self.v + 5*self.v + 140 - self.u + self.I
+            double du = self.a*(self.b*self.v - self.u)
+        self.v += dv*dt
+        self.u += du*dt
+        if self.v >= 30:
+            # Reset at peak of action potential
+            self.v = self.c
+            self.u += self.d
+
+
+def simulate(Neuron neuron, double dt, long n):
+    cdef list record = [None] * n
+    for i in range(n):
+        record[i] = neuron.v
+        neuron.evolve(dt)
+    return record
+
+
+def example():
+    neuron = Neuron(-65, 0, 50, 0.02, 0.2, -65, 2)
+    return simulate(neuron, 0.0001, 1000000)
+
