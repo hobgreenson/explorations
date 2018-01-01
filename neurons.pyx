2cb2e8881ee12e00af9d9f352d844d0a0f0064ef
matthew_green <m.hobson.green@gmail.com>
2017-12-31 21:19:31 -0600

    formatting

--- neurons.pyx
+++ neurons.pyx
@@ -1,10 +1,10 @@
+from libc cimport math
 
 cdef class Neuron:
     """ A biological model neuron introduced by
     Izhikevich (2003).
     """
     cdef double v, u, I, a, b, c, d
-    
     def __init__(self, v, u, I, a, b, c, d):
         # State variables are v (observable membrane potential)
         # and u (hidden recovery variable)
@@ -17,24 +17,30 @@ cdef class Neuron:
         # These variables control dynamics in various ways
         self.a = a
         self.b = b
-        self.c = c
+        self.c = c  # self.v is immediately reset to this value after AP
         self.d = d
+    
+    cpdef double dv(self):
+        """ Returns the time derivative of the membrane potential self.v
+        """
+        return 0.04*self.v*self.v + 5.0*self.v + 140.0 - self.u + self.I
+
+    cpdef double du(self):
+        """ Returns the time derivative of the recovery variable self.u
+        """
+        return self.a*(self.b*self.v - self.u)
 
     cpdef void evolve(self, double dt):
-        """ Updates state variables u and v, using Euler method.
+        """ Updates state variables u and v, using the Euler method.
         """
-        cdef: 
-            double dv = 0.04*self.v*self.v + 5*self.v + 140 - self.u + self.I
-            double du = self.a*(self.b*self.v - self.u)
-        self.v += dv*dt
-        self.u += du*dt
+        self.v += self.dv() * dt
+        self.u += self.du() * dt
         if self.v >= 30:
-            # Reset at peak of action potential
             self.v = self.c
             self.u += self.d
 
 
-def simulate(Neuron neuron, double dt, long n):
+def simulate(Neuron neuron, double dt, unsigned int n):
     cdef list record = [None] * n
     for i in range(n):
         record[i] = neuron.v
